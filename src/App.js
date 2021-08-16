56aebe09a7d857d76a8d6f2aadb4004a269786d6
FaziletKosure <faziletkosure1@gmail.com>
2021-08-16 17:47:58 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+// import Grid from "@material-ui/core/Grid";
+import Header from "./components/Header/Header";
+
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
   return (
     <div>
-      <h1>TRAVEL ADVISOR</h1>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Map />
+        </Grid>
+      </Grid>
     </div>
   );
 };
